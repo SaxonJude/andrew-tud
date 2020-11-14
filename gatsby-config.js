fd1d4e979f9f14e707d5d6cbbff92976aff85cfb
Saxon Du Plooy <saxonjude@gmail.com>
2020-11-14 14:27:58 +0000

    Media

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
       options: {
         fonts: [
           `Montserrat`,
-          `source sans pro\:300,400,400i,700` // you can also specify font weights and styles
+          `source sans pro\:200,300,400,500,600,700` // you can also specify font weights and styles
         ],
         display: 'swap'
       }
